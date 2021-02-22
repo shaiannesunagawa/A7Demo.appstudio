b40722f642bebdbdac5c263846601940d854ff42
shaiannesunagawa <sys74530@creighton.edu>
2021-02-22 15:54:10 -0600

    Initial commit

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,14 @@
+// this is some code to test GHD 
+// and Repository process
+
+
+let myName = "Cindy"
+console.log(`Hello, my name is ${myName}.`)
+console.log(`Welcome to Monday Demos`)
+
+
+/*
+let dogName= ""
+dogName = prompt("Enter the name of your favorite dog: ")
+console.log(`Welcome ${dogName}, it is good to have you here.`)
+*/
