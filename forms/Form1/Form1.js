d6121d3cc8c7ad3748b32213cde3d74016586b4d
shaiannesunagawa <sys74530@creighton.edu>
2021-02-22 15:57:57 -0600

    Update Form1.js

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,11 +1,11 @@
 // this is some code to test GHD 
 // and Repository process
 
-/*
+
 let myName = "Cindy"
 console.log(`Hello, my name is ${myName}.`)
 console.log(`Welcome to Monday Demos`)
-*/
+
 
 /*
 let dogName= ""
